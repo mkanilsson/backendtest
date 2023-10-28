03c57dd6324962739ca7c00b486d1c6b3c3431d9
Marcus Nilsson <marcus.nilsson@genarp.com>
2023-10-28 17:07:29 +0200

    Add tests for Board and Object

--- src/object.rs
+++ src/object.rs
@@ -39,3 +39,94 @@ impl Object {
         board.is_legal_position(&self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::parse_header;
+
+    use super::*;
+
+    #[test]
+    fn move_forward_when_direction_is_north() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        assert!(object.move_forward(&board));
+        assert_eq!(object.x, 1);
+        assert_eq!(object.y, 0);
+    }
+
+    #[test]
+    fn move_backward_when_direction_is_north() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        assert!(object.move_backward(&board));
+        assert_eq!(object.x, 1);
+        assert_eq!(object.y, 2);
+    }
+
+    #[test]
+    fn move_forward_when_direction_is_east() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        object.direction = Direction::East;
+
+        assert!(object.move_forward(&board));
+        assert_eq!(object.x, 2);
+        assert_eq!(object.y, 1);
+    }
+
+    #[test]
+    fn move_backward_when_direction_is_east() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        object.direction = Direction::East;
+
+        assert!(object.move_backward(&board));
+        assert_eq!(object.x, 0);
+        assert_eq!(object.y, 1);
+    }
+
+    #[test]
+    fn move_forward_when_direction_is_west() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        object.direction = Direction::West;
+
+        assert!(object.move_forward(&board));
+        assert_eq!(object.x, 0);
+        assert_eq!(object.y, 1);
+    }
+
+    #[test]
+    fn move_backward_when_direction_is_west() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        object.direction = Direction::West;
+
+        assert!(object.move_backward(&board));
+        assert_eq!(object.x, 2);
+        assert_eq!(object.y, 1);
+    }
+
+    #[test]
+    fn move_backward_when_direction_is_south() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        object.direction = Direction::South;
+
+        assert!(object.move_forward(&board));
+        assert_eq!(object.x, 1);
+        assert_eq!(object.y, 2);
+    }
+
+    #[test]
+    fn move_forward_when_direction_is_south() {
+        let (board, mut object) = parse_header("4,4,1,1");
+
+        object.direction = Direction::South;
+
+        assert!(object.move_backward(&board));
+        assert_eq!(object.x, 1);
+        assert_eq!(object.y, 0);
+    }
+}
